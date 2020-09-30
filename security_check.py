60ae2e1d512cb64784816cb3aced81f5275aa282
Ben Millington Drake <ben6931@mc80a0l415.rackspace.corp>
2020-09-30 20:13:21 +0400

    initial commit for security check

---
+++ security_check.py
@@ -0,0 +1,377 @@
+#!/usr/bin/python
+
+"""
+This script can be used to check the encrpytion status of EC2 EBS volumes
+
+Executing the script will check all EC2 instances on whichever account you are currently authenticated. 
+
+Developed By - ben.millingtondrake@rackspace.com
+
+
+"""
+
+import boto3
+from botocore.exceptions import ClientError
+from pprint import pprint as pp
+
+REGION = "eu-central-1"
+
+# Setup clients
+ec2 = boto3.client('ec2')
+s3 = boto3.client('s3')
+
+# initialise / declare required vars
+CHECKED_RESOURCES = [
+    'AWS::ElasticLoadBalancingV2::LoadBalancer',
+    'AWS::S3::Bucket',
+    'AWS::EC2::Instance',
+    'AWS::RDS::DBInstance'
+]
+
+INSECURE_SSL_CIPHERS = [
+    "Protocol-TLSv1",
+    "Protocol-TLSv1.1"
+]
+
+INSTANCES = []
+EBS_DEVICES = []
+USED_REGIONS = []
+S3_BUCKETS = []
+LOAD_BALANCERS = [] 
+
+def get_all_instances():
+
+    """ Get all ec2 instances in a given region """
+
+    global INSTANCES
+
+    try:
+        token = ""
+        while True:
+            response = ec2.describe_instances(NextToken=token)
+            INSTANCES += response['Reservations']
+
+            if 'NextToken' in response:
+                token = response['NextToken']
+            else:
+                break
+        return True
+        
+    except ClientError as e:
+        
+        print (e)
+
+def get_instance_block_device_mappings():
+
+    """ Get ebs devices from the ec2 response """
+    
+    global INSTANCES
+    global EBS_DEVICES
+
+    for instance in INSTANCES:
+        devices = []
+        name_tag = "NO_NAME_TAG"
+
+
+        for dev in instance['Instances'][0]['BlockDeviceMappings']:
+            ebs_vol_id = dev['Ebs']['VolumeId']
+            vol_arrtib = get_ebs_volume_attrib(ebs_vol_id)
+
+            devices.append({
+                "DeviceName": dev['DeviceName'],
+                "VolumeId": ebs_vol_id,
+                "Encrypted": vol_arrtib['Encrypted']
+            })
+
+        for t in instance['Instances'][0]['Tags']:
+            if t['Key'] == "Name":
+                name_tag = t["Value"]
+
+        EBS_DEVICES.append({
+            "name": name_tag if name_tag else "",
+            "instance_id": instance['Instances'][0]["InstanceId"],
+            "ebs_root": instance['Instances'][0]['RootDeviceName'],
+            "ebs_optimised": instance['Instances'][0]['EbsOptimized'],
+            "ebs_devices": devices
+        })
+        
+    return True
+
+def get_ebs_volume_attrib(vol_id):
+
+    """ get ebs volume attributes """
+
+    try:
+        volume = ec2.describe_volumes(
+            VolumeIds=[
+                vol_id,
+            ],
+        )
+
+        return(volume['Volumes'][0])
+        
+    except ClientError as e:
+        print (e)
+
+def populate_used_regions():
+
+    """ 
+    Use AWS config to get resources in each region and confirm which are in use
+    (based on the resources we are looking for)
+    """
+
+    global USED_REGIONS
+    discovered_regions = set()
+
+    try: 
+        all_regions = ec2.describe_regions()
+    except ClientError as e:
+        print (e)
+        return(1)
+
+    for region in all_regions['Regions']:
+        try:
+            # Use AWS config to list all resources in the region
+            config_client = boto3.client('config', region['RegionName'])
+            discovered_rescources = config_client.get_discovered_resource_counts()
+        
+        except ClientError as e:
+            print (e)
+
+        for resource in discovered_rescources['resourceCounts']:
+            for monitored_resource in CHECKED_RESOURCES:
+                # If a discovered resource exists in our list of checked resources then add the region to a list with the associated resources
+                if monitored_resource == resource['resourceType'] and region['RegionName'] not in discovered_regions:
+                    USED_REGIONS.append({
+                        "region": region['RegionName'],
+                        "resources": discovered_rescources['resourceCounts']
+                    })
+                    discovered_regions.add(region['RegionName'])
+
+    return
+
+def perform_security_checks():
+
+    """ Run functions for running security checks """
+
+    global USED_REGIONS
+
+    # Kick off security checks for each region and resource type
+    for region in USED_REGIONS:
+        for resource in region['resources']:
+            if resource['resourceType'] == 'AWS::EC2::Instance':
+                check_ec2(region['region'])
+
+            if resource['resourceType'] == 'AWS::S3::Bucket' and not S3_BUCKETS:
+                check_s3()
+
+            if resource['resourceType'] == 'AWS::ElasticLoadBalancingV2::LoadBalancer':
+                # print(region['region'])
+                check_alb(region['region'])
+
+            if resource['resourceType'] == 'AWS::RDS::DBInstance':
+                # print(region['region'])
+                check_rds(region['region'])
+    return
+
+def get_all_s3():
+
+    """ Pull every S3 bucket and add to array """
+
+    global S3_BUCKETS
+
+    try:
+        response = s3.list_buckets()
+        for bucket in response['Buckets']:
+            S3_BUCKETS.append({
+                "bucket_name": bucket['Name'],
+                "encrypted": "false"
+            })
+
+        return True
+        
+    except ClientError as e:
+        print (e)
+
+    return
+
+def check_s3_encryption():
+
+    """ Check encryption of each S3 bucket """
+
+    global S3_BUCKETS
+    buckets_encrypted = []
+
+    try:
+        for bucket in S3_BUCKETS:
+            try:
+                s3.get_bucket_encryption(Bucket=bucket['bucket_name'])
+                buckets_encrypted.append({
+                    "bucket_name": bucket['bucket_name'],
+                    "encrypted": "true"
+                })
+            
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+                    buckets_encrypted.append({
+                        "bucket_name": bucket['bucket_name'],
+                        "encrypted": "false"
+                    })
+                else:
+                    print (e)
+
+    except ClientError as e:
+        print (e)
+
+    S3_BUCKETS = buckets_encrypted
+
+    return
+
+def get_load_balancers(region):
+
+    """ 
+    Get all LB's and add to list 
+
+    LOAD_BALANCERS is the object which will end up populated. 
+    
+    This function creates objects which ultimately get added to that list. 
+
+    Object dict:
+    -------------
+    LB_TYPE : < elb/alb >,
+    LB_NAME : < Name of LB >,
+    LISTENERS : [
+        LISTENER_NAME : < Name of Listener >,
+        REFERENCE_POLICY : < Name of policy >, # Un-needed but good for debug
+        CIPHERS : [
+            < List of ciphers in use >
+        ]
+    ]
+
+    lb_object           == the whole object dict
+    lb_listener_object  == objects created by loop and appended to lb_listeners list. 
+    lb_listener_ciphers == list of ciphers used by the listener. Appended to lb_listener_object['CIPHERS']
+
+    """
+
+
+    global LOAD_BALANCERS
+
+    elb = boto3.client('elb', region)
+    elbv2 = boto3.client('elbv2', region)
+
+    try:
+        current_elb = elb.describe_load_balancers()
+        current_alb = elbv2.describe_load_balancers()
+        
+    except ClientError as e:
+        print (e)
+
+    if len(current_elb['LoadBalancerDescriptions']) > 0:
+        for lb in current_elb['LoadBalancerDescriptions']:
+            # initialise the lb_object, append the type and name properties, and initialise the LISTENERS property list
+            lb_object = {}
+            lb_object['LB_TYPE'] = "elb"
+            lb_object['LB_NAME'] = lb['LoadBalancerName']
+            lb_object['LISTENERS'] = []
+
+            # get all listeners on the ELB
+            for listener in lb['ListenerDescriptions']:
+                lb_listener_object = {}
+
+                for listener_policy_name in listener['PolicyNames']:
+                    lb_listener_object['POLICY_NAME'] = listener_policy_name
+
+                    policies = elb.describe_load_balancer_policies(
+                        LoadBalancerName=lb_object['LB_NAME'],
+                        PolicyNames=[
+                            listener_policy_name
+                        ]
+                    )
+
+
+                    lb_listener_ciphers = []
+                    # loop through configured ciphers, make note of reference policy if there is one and record all ciphers in use
+                    for cipher in policies['PolicyDescriptions'][0]['PolicyAttributeDescriptions']:
+                        if 'AttributeValue' in cipher and cipher['AttributeName'] == "Reference-Security-Policy":
+                            lb_listener_object['REFERENCE_POLICY'] = cipher['AttributeValue']
+
+                        if 'AttributeValue' in cipher and cipher['AttributeValue'] == "true":
+                            lb_listener_ciphers.append(cipher['AttributeName'])
+                    
+                    # add ciphers to the listener object and then add the object to the lb_object. 
+                    lb_listener_object['CIPHERS'] = lb_listener_ciphers
+                    lb_object['LISTENERS'].append(lb_listener_object)
+
+            # once all listener objects have been added to the lb_object, add that to the main LOAD_BALANCERS list
+            LOAD_BALANCERS.append(lb_object)
+
+        pp(LOAD_BALANCERS)
+        return
+
+    # if len(current_alb['LoadBalancers']) > 0:
+    #     print(f"alb: {len(current_alb['LoadBalancers'])}")
+    #     # pp(current_alb['LoadBalancers'])
+
+    #     for lb in current_alb['LoadBalancers']:
+    #         pp(lb)
+    #         # print(lb['LoadBalancerName'])
+
+    
+
+
+    # pp(current_elb['LoadBalancers'])
+    # if len(current_elb['LoadBalancers']) == 0:
+        
+
+
+    return
+
+def check_ec2(region):
+    return
+    # Overwrite global ec2 object and update region
+    global ec2
+    ec2 = boto3.client('ec2', region)
+    get_all_instances()
+    get_instance_block_device_mappings()
+    return
+
+def check_s3():
+    return
+    get_all_s3()
+    check_s3_encryption()
+    return
+
+def check_alb(region):
+    get_load_balancers(region)
+    # print("Checking alb")
+    return
+
+def check_rds(region):
+    # print("Checking rds")
+    return
+
+def print_details():
+    global EBS_DEVICES
+
+    for instance in EBS_DEVICES:
+        print(f'{instance["name"]}')
+        print(f'\tInstance ID:\t{instance["instance_id"]}')
+        print(f'\tEBS Root:\t{instance["ebs_root"]}')
+        print(f'\tEBS Optimised:\t{instance["ebs_optimised"]}')
+        print(f'\tEBS Devices:')
+        print(f'\t\tDevice\t\tVolume ID\t\tEncrypted')
+        for ebs in instance['ebs_devices']:
+            print(f'\t\t{ebs["DeviceName"]}\t{ebs["VolumeId"]}\t{ebs["Encrypted"]}')
+
+        print('\n')
+        
+def main():
+    populate_used_regions()
+    perform_security_checks()
+    # print_details()
+
+## Currently working load balancer parsing
+
+if __name__ == "__main__":
+    main()
